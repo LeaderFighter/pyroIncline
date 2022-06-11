3f40b22462c2e6ff5725d73bddd418c85cec18f2
LeaderFighter <87716278+LeaderFighter@users.noreply.github.com>
2022-06-12 02:37:58 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -24,112 +24,34 @@ MESSAGE2 = config("MESSAGE2", default=None)
 
 MESSAGE3 = config("MESSAGE3", default=None)
 
-api_id = 2184829
-api_hash = "6930b92388baabff4cb4a1d377085035"
+API_ID = 2184829
+API_HASH = "6930b92388baabff4cb4a1d377085035"
 
 
 
 async def main():
-    async with Client(session_name=STRING_SESSION, API_ID, API_HASH) as app:
+    async with Client(name=STRING_SESSION, API_ID, API_HASH) as app:
         await app.send_message("me", "Bot Deployed Successfully!")
-        
-        for i in range(COUNT):
-            spam = [
-            MESSAGE1,
-            MESSAGE2,
-            MESSAGE3
-            ]
-            
-            myChoice = random.choice(spam)
-      
-            await app.send_chat_action(str(CHATID), enums.ChatAction.TYPING)import asyncio
-
-import os
-
-import time
-
-import logging
-
-from decouple import config
-
-from pyrogram import Client
-
-from pyrogram import enums
-
-import random
-
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-
-                    level=logging.WARNING)
-
-STRING_SESSION = config("STRING_SESSION", default=None)
-
-COUNT = config("COUNT", default=None)
-
-CHATID = config("CHATID", default=None)
-
-MESSAGE1 = config("MESSAGE1", default=None)
-
-MESSAGE2 = config("MESSAGE2", default=None)
-
-MESSAGE3 = config("MESSAGE3", default=None)
-
-api_id = 2184829
-
-api_hash = "6930b92388baabff4cb4a1d377085035"
-
-async def main():
-
-    async with Client(session_name=STRING_SESSION, API_ID, API_HASH) as app:
-
-        await app.send_message("me", "Bot Deployed Successfully!")
-
         print ("Bot Deployed To Spam!!!")
-
         
-
         try:
-
             for i in range(COUNT):
-
                 spam = [
-
                 MESSAGE1,
-
                 MESSAGE2,
-
                 MESSAGE3
-
                 ]
-
                 
-
                 myChoice = random.choice(spam)
-
                 try:
-
                     await app.send_chat_action(str(CHATID), enums.ChatAction.TYPING)
-
                     await app.send_message(CHATID, myChoice)
-
                     await app.send_chat_action(str(CHATID), enums.ChatAction.TYPING)
-
                     await asyncio.sleep(0.4)
-
                 except Exception as e:
-
                     print(e)
-
         except Exception as e:
-
             print(e)
-
-    app.run()
-
-asyncio.run(main())
-            await app.send_message(CHATID, myChoice)
-            await app.send_chat_action(str(CHATID), enums.ChatAction.TYPING)
-            await asyncio.sleep(0.4)
 
     app.run()
 asyncio.run(main())
